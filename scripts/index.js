debe1a892e4ba25fb51b0fc65bbd397ffcacdfb0
laura <lauraochoa.acuna@gmail.com>
2025-01-20 20:44:37 -0500

    second

--- scripts/index.js
+++ scripts/index.js
@@ -11,23 +11,17 @@ const editAbout = document.getElementById("about");
 saveButton.addEventListener("click", save);
 function save(e) {
   e.preventDefault();
-  console.log("le di click al save");
   editProfileName.textContent = editName.value;
   editInfo.textContent = editAbout.value;
   closePopUp();
 }
 editButton.addEventListener("click", openPopUp);
 function openPopUp() {
-  console.log("feliz aprendiendo js");
   popup.classList.add("popup__visible");
 }
 closeButton.addEventListener("click", closePopUp);
 function closePopUp() {
-  console.log("feliz aprendiendo js");
   popup.classList.remove("popup__visible");
 }
 const addButton = document.getElementById("add-button"); //document.querySelector("#edit-button"")
 addButton.addEventListener("click", openPop);
-function openPop() {
-  console.log("HOLA Victor si estoy entendiendo");
-}
