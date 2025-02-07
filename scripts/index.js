d95493de7b91fe5b73051e913ebec93ec829423a
laura <lauraochoa.acuna@gmail.com>
2025-02-07 14:51:42 -0500

    trying

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,122 @@
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
+    alt: "Valle de Yosemite",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
+    alt: "Lago Louise",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
+    alt: "Montañas Calvas",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
+    alt: "Latemar",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
+    alt: "Parque Nacional de la Vanoise",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
+    alt: "Lago di Braies",
+  },
+];
+
 const editButton = document.getElementById("edit-button"); //document.querySelector("#edit-button"")
-const popup = document.getElementById("popup");
+const popUps = document.querySelectorAll(".popup");
 const closeButton = document.getElementById("close-button");
 const saveButton = document.getElementById("save");
+const addButton = document.getElementById("add-button");
+const popUpImage = document.getElementById("popup__image");
+
+const likeButtons = document.querySelectorAll(".elements__like-button");
+const popUpPlace = document.getElementById("popup__new-place");
+const popUpPerson = document.getElementById("popup__Person");
+const handlePopUpClose = (evt) => {};
 
 const editProfileName = document.getElementById("profile-name");
 const editInfo = document.getElementById("profile-info");
 const editName = document.getElementById("name");
 const editAbout = document.getElementById("about");
-
-saveButton.addEventListener("click", save);
+const cardTemplate = document.getElementById("cardtemplate").content.querySelector(".elements__card"); //content inside template
+const cardsContainer = document.getElementById("elements");
 function save(e) {
   e.preventDefault();
   editProfileName.textContent = editName.value;
   editInfo.textContent = editAbout.value;
   closePopUp();
 }
-editButton.addEventListener("click", openPopUp);
+//function to open popups
 function openPopUp() {
-  popup.classList.add("popup__visible");
+  popUps.classList.add("popup__visible");
 }
-closeButton.addEventListener("click", closePopUp);
+function openPopUpPerson() {
+  popUpPerson.classList.add("popup__visible");
+}
+function openPopUpPlace() {
+  popUpPlace.classList.add("popup__visible");
+}
+
+function openPopUpImage(data, name) {
+  popUpImage.classList.add("popup__visible");
+  Image = popUpImage.querySelector(".popupimage");
+  Image.src = data.link;
+}
 function closePopUp() {
-  popup.classList.remove("popup__visible");
+  popUps.classList.remove("popup__visible");
 }
-const addButton = document.getElementById("add-button"); //document.querySelector("#edit-button"")
-addButton.addEventListener("click", openPop);
+// Toggle the "liked" class which changes the color
+function like() {
+  like.styleBackgroundColor = "black";
+}
+//function to close popups
+popUps.forEach((popup) => {
+  const button = popup.querySelector(".popup__close-button");
+  button.addEventListener("click", () => {
+    popup.classList.remove("popup__visible");
+  });
+});
+
+saveButton.addEventListener("click", save);
+editButton.addEventListener("click", openPopUpPerson);
+addButton.addEventListener("click", openPopUpPlace);
+
+//document.querySelector("#edit-button"")
+initialCards.forEach((data) => {
+  renderCard(data, cardsContainer);
+});
+function renderCard(data, cardsContainer) {
+  cardsContainer.prepend(getCardElement(data));
+}
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardTitle = cardElement.querySelector(".elements__title");
+  const likeButton = cardElement.querySelector(".elements__like-button");
+  const cardImg = cardElement.querySelector(".elements__card-photo");
+  const trashButton = cardElement.querySelector(".elements__trash-button");
+  const popUpCaption = cardElement.querySelector(".popup__caption");
+
+  cardImg.addEventListener("click", () => openPopUpImage(data));
+
+  //function too delete each card
+  cardImg.src = data.link;
+  cardTitle.textContent = data.name;
+  trashButton.addEventListener("click", (evt) => {
+    evt.target.closest(".elements__card").remove();
+  });
+  //function to like a card
+  likeButton.addEventListener("click", () => {
+    likeButton.classList.toggle("element__like-button_is-active");
+  });
+  return cardElement;
+}
